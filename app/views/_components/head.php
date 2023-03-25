816bebda0ddf95c9f7f53069d4aa370a41ac2610
astepanenko <astepanenko@ves-media.com>
2023-03-25 18:41:27 +0300

    hack files fix

--- app/views/_components/head.php
+++ app/views/_components/head.php
@@ -3,10 +3,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="astepanenko (email me at astepanenko.turkiye@gmail.com)">
-    <title></title>
+    <title><?=$title?></title>
     <meta name="description" content="" />
     <meta property="og:url" content="" />
-    <meta property="og:title" content="" />
+    <meta property="og:title" content="<?=$title?>" />
     <meta property="og:description" content="" />
     <meta property="og:type" content="website" />
     <meta property="og:image" content="" />
